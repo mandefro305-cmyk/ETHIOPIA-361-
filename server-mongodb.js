dcb53105b59177abe65d0ea0e4093b9d5c960f03
mandefro305-cmyk <mandefro305@gmail.com>
2026-03-30 20:47:45 +0300

    Set view engine and views directory for EJS

--- server-mongodb.js
+++ server-mongodb.js
@@ -57,8 +57,8 @@ const upload = multer({
 });
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(session({
     secret: 'ethiopia-tourism-secret',
